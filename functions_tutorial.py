c459aaf598dcbd2d17d4d4ab5a6c6970cdf05474
Deiv101 <hrakojoana@yahoo.com>
2021-11-11 11:02:29 +0200

    "First commit"

---
+++ functions_tutorial.py
@@ -0,0 +1,13 @@
+# Creating a function
+def my_function():
+    print("Hello from a function")
+
+# Calling a Function
+my_function
+
+# Arguments
+def my_function(fname):
+    print(fname + " Rakojoana")
+my_function("David")
+my_function("Lilly")
+my_function("Amo")
